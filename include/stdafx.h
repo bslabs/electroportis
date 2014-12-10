b1359fb495b8486273553fafdddef668342962e6
Brendan Shanks <brendan@bslabs.net>
2014-12-09 20:13:42 -0800

    Non-Windows: Add empty stdafx.h
    ep.c has to #include <stdafx.h> in order for precompiled headers to work on MSVC, add an empty stdafx.h so non-MSVC are happy

---
+++ include/stdafx.h
@@ -0,0 +1,5 @@
+/* Empty stdafx.h to be used for non-MSVC compilers
+ *
+ * On MSVC the Windows-specific include directory (containing a real stdafx.h)
+ * is listed first, and that stdafx.h is used instead of this one
+ */

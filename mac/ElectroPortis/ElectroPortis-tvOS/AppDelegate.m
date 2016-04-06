7706a4e86c7261433ebb4673bc6d3afe8c6cf0e6
Brendan Shanks <brendan@bslabs.net>
2016-04-05 23:45:19 -0700

    tvOS: Prevent screensaver from running

--- mac/ElectroPortis/ElectroPortis-tvOS/AppDelegate.m
+++ mac/ElectroPortis/ElectroPortis-tvOS/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [application setIdleTimerDisabled:YES]; // Prevent screensaver from starting
     return YES;
 }
 

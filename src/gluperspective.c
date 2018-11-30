6a6e175261d0961ec5df90515311dadd64d7c7bb
Brendan Shanks <brendan@bslabs.net>
2018-11-30 00:10:37 -0800

    gluPerspective: Define M_PI if system headers don't (needed for CodeWarrior Pro 8)

--- src/gluperspective.c
+++ src/gluperspective.c
@@ -5,6 +5,11 @@
 #define _USE_MATH_DEFINES // needed for MSVC to define M_PI
 #include <math.h>
 
+/* some math.h's don't define M_PI */
+#ifndef M_PI
+#define M_PI 3.14159265359
+#endif
+
 void ep_gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar)
 {
   GLdouble xmin, xmax, ymin, ymax;
